64d3e40013c26fd6e487b09482d3af6a31e5c2a3
Jell <jean-louis@jawaninja.com>
2016-04-15 11:52:09 +0200

    Sudoku core.logic

---
+++ 2012-12-11_sudoku-in-logic-programming/README.md
@@ -0,0 +1,11 @@
+# sudoku-core-logic
+
+http://www.meetup.com/got-lambda/events/91567852/
+
+Sudoku solver implemented using core.logic
+
+## License
+
+Copyright © 2012 FIXME
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ 2012-12-11_sudoku-in-logic-programming/src/sudoku_core_logic/core.clj
@@ -0,0 +1,41 @@
+(ns sudoku-core-logic.core
+  (:refer-clojure :exclude [==])
+  (:use clojure.core.logic))
+
+(defn get-squares [cells]
+  (->> cells
+       (partition 3)
+       (partition 3)
+       (apply interleave)
+       (partition 3)
+       (map flatten)))
+
+(defn solve-sudoku [constraints]
+  (let [sudoku-domain (domain 1 2 3 4 5 6 7 8 9)
+        cells         (repeatedly 81 lvar)
+        rows          (mapv vec (partition 9 cells))
+        columns       (apply mapv vector rows)
+        squares       (get-squares cells)
+        constraints   (map #(if (zero? %) (lvar) %)
+                           (flatten constraints))]
+    (first
+     (run 1 [q]
+       (everyg #(infd % sudoku-domain) cells)
+       (== cells constraints)
+       (== q rows)
+       (everyg distinctfd rows)
+       (everyg distinctfd columns)
+       (everyg distinctfd squares)))))
+
+(def super-hard-sudoku
+  [[8 0 0  0 0 0  0 0 0]
+   [0 0 3  6 0 0  0 0 0]
+   [0 7 0  0 9 0  2 0 0]
+
+   [0 5 0  0 0 7  0 0 0]
+   [0 0 0  0 4 5  7 0 0]
+   [0 0 0  1 0 0  0 3 0]
+
+   [0 0 1  0 0 0  0 6 8]
+   [0 0 8  5 0 0  0 1 0]
+   [0 9 0  0 0 0  4 0 0]])
